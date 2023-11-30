f9120b50e10d661fb2fd6d1e1ad9b95f36872963
Jayden Soh <delayedjayden@gmail.com>
2023-11-30 23:34:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+from colorama import Fore
+import networkx as nx
+
+z = {1: {"br": False,
+         "bl": True}}
+
 x = 1
 while True:
     
@@ -8,4 +14,21 @@ while True:
     x += 1
     print(x)
     
-print("breaked")
+
+print(Fore.RESET + Fore.RED +"breaked"+Fore.RESET)
+print("Hello")
+
+map = nx.Graph()
+map.add_node(1, br=True, bl=False)
+print(nx.get_node_attributes(map, "br"))
+print(nx.get_node_attributes(map, "bl"))
+nx.set_node_attributes(map, False, "br")
+print(nx.get_node_attributes(map, "br"))
+print(nx.get_node_attributes(map, "bl"))
+
+print(z)
+z[0]
+y = z[1]
+y["bl"] = True
+y["b0"] = True
+print(z)
